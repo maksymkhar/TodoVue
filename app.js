9a0d2678e37812781ea8dd964a7a854d15c5fd26
Max <maksymkharuk@iesebre.com>
2015-11-05 21:08:33 +0100

    Added remove/edit functionality, added css source

--- app.js
+++ app.js
@@ -8,7 +8,48 @@ new Vue({
 
     data: {
 
-        heading: 'Hello!'
+        tasks: [
+
+            { body: 'Go to the store', completed: false}
+        ],
+
+        newTask: ''
+
+    },
+
+    methods: {
+
+        addTask : function (e)
+        {
+            e.preventDefault();
+
+            this.tasks.push({
+
+                body: this.newTask,
+                completed: false
+            });
+
+            this.newTask = '';
+        },
+        removeTask: function(task)
+        {
+            this.tasks.$remove(task);
+        },
+        editTask: function(task)
+        {
+            //remove task
+
+            this.removeTask(task);
+
+            //update new task
+
+            this.newTask = task.body;
+
+            //focus new task inoput
+
+            this.$$.newTask.focus();
+        }
+
     }
 
 })
